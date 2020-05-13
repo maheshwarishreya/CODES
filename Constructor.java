d49c2cccfa81d2389585cbdc76de10e3b16fcbcc
1Shreya123 <maheshwarishreya99@gmail.com>
2020-05-13 23:14:57 +0530

    This is a constructor file

---
+++ Constructor.java
@@ -0,0 +1,22 @@
+
+ class Constructor{
+	int id;
+	String name;
+	double marks;
+	Constructor(int ii,String st,double m)
+	{
+		id=ii;
+	    name=st;
+	}	
+	marks=m;
+ void display()
+ {
+	 System.out.println(id+" "+name+" "+marks);
+ }
+ public static void main(String args[])
+ {
+	 Constructor ob=new Constructor(10,"shreya",20.9);
+	 ob.display();
+ }
+}
+ 
